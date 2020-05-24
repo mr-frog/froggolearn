5c6f2d34fd74039a326494aa5e21c0b2281f6fc5
Mr-Frog <niklas.frerichs@gmail.com>
2020-05-24 16:36:17 +0200

    some systematic changes, comments for anomaly detection models

--- froggolearn/anomaly/gaussian.py
+++ froggolearn/anomaly/gaussian.py
@@ -1,7 +1,6 @@
 import numpy as np
-from numpy.linalg import det, slogdet, pinv
 from scipy.stats import multivariate_normal
-import sys
+
 class GaussianAnomalyDetection:
     def __init__(self, cutoff='auto'):
         """
@@ -11,31 +10,63 @@ class GaussianAnomalyDetection:
         """
         self.cutoff = cutoff
 
-    def detect(self, X):
+    def fit(self, X):
         """
-        Returns a label for each feature vector in X, where 1 indicates
-        an outlier, and 0 a non-outlier.
+        Fits the parameters of a gaussian distribution describing the
+        dataset X.
         """
-        X = X.copy()
-        mu = np.mean(X, axis=0)
-        sig = np.var(X, axis=0)
-        pre_exp = (1 / (np.sqrt(2 * np.pi) * sig))
-        exp = np.exp(-(X - mu)**2 / (2 * sig**2))
-        prob = np.prod(pre_exp*exp, axis=1)
+        self.mu = np.mean(X, axis=0)
+        self.sigma = np.var(X, axis=0)
+
+    def predict(self, X):
+        """
+        Returns labels [1, 0] for [outlier, inlier].
+        """
+        prob = self.probability(X)
         if self.cutoff == 'auto':
-            self.cutoff = np.mean(prob) - np.std(prob)
+            self.cutoff = np.mean(prob) - 2 * np.std(prob)
         return np.array([prob < self.cutoff])
+
+    def probability(self, X):
+        """
+        Returns the probability of occurance in the gaussian distribution
+        for each element in a given dataset X.
+        """
+        pre_exp = (1 / (np.sqrt(2 * np.pi) * self.sigma))
+        exp = np.exp(-(X - self.mu)**2 / (2 * self.sigma**2))
+        return np.prod(pre_exp*exp, axis=1)
+
 
 class MultivariateGaussian:
     def __init__(self, cutoff='auto'):
+        """
+        Simple Gaussian Anomaly Detection System
+        If cutoff = 'auto' uses some very simple math to decide the cutoff for
+        labeling outliers.
+        """
         self.cutoff = cutoff
 
-    def detect(self, X):
-        m = X.shape[0]
+    def fit(self, X):
+        """
+        Fits the parameters of a multivariate gaussian distribution describing
+        the dataset X using scipy.stats.multivariate_normal.
+        """
         mu = np.mean(X, axis=0)
         Sigma = np.cov(X.T)
-        _prob = multivariate_normal(mean=mu, cov=Sigma)
-        prob = _prob.pdf(X)
+        self.mvg = multivariate_normal(mean=mu, cov=Sigma)
+
+    def predict(self, X):
+        """
+        Returns labels [1, 0] for [outlier, inlier].
+        """
+        prob = self.mvg.pdf(X)
         if self.cutoff == 'auto':
-            self.cutoff = np.mean(prob) - np.std(prob)
+            self.cutoff = np.mean(prob) - 2 * np.std(prob)
         return np.array([prob < self.cutoff])
+
+    def probability(self, X):
+        """
+        Returns the probability of occurance in the gaussian distribution
+        for each element in a given dataset X.
+        """
+        return self.mvg.pdf(X)
